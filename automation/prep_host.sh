2d95aec6fdc6f6333fe4b94946e92039e306b4cc
safemoad <safemoad+github@proton.me>
2025-06-14 09:51:20 -0700

    Update prep_host.sh

--- automation/prep_host.sh
+++ automation/prep_host.sh
@@ -2,15 +2,13 @@
 
 # Update OS and Packages
 read -n 1 -p 'Step 1 of 3: Run APT Update/Upgrade on this machine? [Y/n] ' response
-  if [ "$response" != "Y" ] 
-    then 
-      echo ''
-      echo '***Skipping APT Update/Upgrade*** '
-    fi
   if [ "$response" == "Y" ] 
     then
       echo ''
       echo 'Step 1 of 3: Updating and upgrading machine, please wait... ' && yes | sudo apt update > /dev/null 2>&1 && yes | sudo apt upgrade > /dev/null 2>&1
+    else
+      echo ''
+      echo '***Skipping APT Update/Upgrade*** '
     fi
 
 # Update Searchsploit Database
