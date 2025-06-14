fb822ea00fc4a5d784cf2e6e1aa1e538cfcabc9c
safemoad <safemoad+github@proton.me>
2025-06-14 08:35:37 -0700

    Update prep_vm.sh

--- automation/prep_vm.sh
+++ automation/prep_vm.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/bash
 
 # Update VM
-echo 'Updating and upgrading VM..."
+echo 'Updating and upgrading VM... "
 sudo apt update  > /dev/null 2>&1 && sudo apt safe-upgrade  > /dev/null 2>&1
 
 # Update Searchsploit
-echo 'Updating Searchsploit..."
+echo 'Updating Searchsploit... "
 searchsploit -u > /dev/null 2>&1
 
 # Update Locate Database
-echo 'Updating Locate database..."
+echo 'Updating Locate database... "
 sudo updatedb
