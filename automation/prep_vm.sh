3548633b14510cd3568f274aff2270441d3e0e50
safemoad <safemoad+github@proton.me>
2025-06-14 09:01:33 -0700

    Update prep_vm.sh

--- automation/prep_vm.sh
+++ automation/prep_vm.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/bash
 
 # Update VM
-echo 'Updating and upgrading VM...'
-sudo apt update && sudo apt safe-upgrade
+echo 'Step 1 of 3: Updating and upgrading VM, please wait...'
+yes | sudo apt update > /dev/null 2>&1 && yes | sudo apt safe-upgrade > /dev/null 2>&1 
 
 # Update Searchsploit
-echo 'Updating Searchsploit database...'
+echo 'Step 2 of 3: Updating Searchsploit database, please wait...'
 searchsploit -u > /dev/null 2>&1
 
 # Update Locate Database
-echo 'Updating Locate database...'
+echo 'Step 3 of 3: Updating Locate database, please wait...'
 sudo updatedb
