2303550839d46522b2b301c4e078fcbbb8c1fd56
safemoad <safemoad+github@proton.me>
2025-06-14 09:44:47 -0700

    Update prep_vm.sh

--- automation/prep_vm.sh
+++ automation/prep_vm.sh
@@ -9,12 +9,13 @@ read -n 1 -p 'Step 1 of 3: Run APT Update/Upgrade on this machine? [Y/n] ' respo
     fi
   if [ "$response" == "Y" ] 
     then
-      echo 'Step 1 of 3: Updating and upgrading VM, please wait... ' && yes | sudo apt update && yes | sudo apt upgrade
+      echno ''
+      echo 'Step 1 of 3: Updating and upgrading VM, please wait... ' && yes | sudo apt update > /dev/null 2>&1 && yes | sudo apt upgrade > /dev/null 2>&1
     fi
 
 # Update Searchsploit Database
 echo 'Step 2 of 3: Updating Searchsploit database, please wait... '
-searchsploit -u #> /dev/null 2>&1
+searchsploit -u > /dev/null 2>&1
 
 # Update Locate Database
 echo 'Step 3 of 3: Updating Locate database, please wait... '
