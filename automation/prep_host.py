6b24b02125a822f625803ebcd20b0e46e8a9ceb7
safemoad <safemoad+github@proton.me>
2025-06-14 17:26:06 -0700

    Update prep_host.py

--- automation/prep_host.py
+++ automation/prep_host.py
@@ -7,16 +7,14 @@ def update_apt():
     response = input("Step 1 of 3: Run APT Update/Upgrade on this machine? [Y/n]")
     if response == "Y":
         print(colored("Step 1 of 3: Updating and upgrading machine, please wait...", "white"))
-        process = subprocess.run(["sudo apt update -y && sudo apt upgrade -y"], shell=True, capture_output=True, text=True)
-        print(colored((process.stderr), "red"))
+        process = subprocess.run(["sudo apt update -y && sudo apt upgrade -y"], shell=True)
     else:
         print(colored("***Skipping APT Update/Upgrade***", "green"))
 
 #Update Searchsploit Database
 def update_searchsploit():
     print(colored("Step 2 of 3: Updating Searchsploit database, please wait...", "white"))
-    process = subprocess.run(["searchsploit -u"], shell=True, capture_output=True, text=True)
-    print(colored((process.stderr), "red"))
+    process = subprocess.run(["searchsploit -u"], shell=True)
 
 # Update Locate Database
 def update_locate():
