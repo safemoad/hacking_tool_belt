d35ebe905b7319d64e4b86926c5de1e3e5c0d58b
safemoad <safemoad+github@proton.me>
2025-06-14 17:09:58 -0700

    Update prep_host.py

--- automation/prep_host.py
+++ automation/prep_host.py
@@ -7,7 +7,7 @@ def update_apt():
     response = input("Step 1 of 3: Run APT Update/Upgrade on this machine? [Y/n]")
     if response == "Y":
         print(colored("Step 1 of 3: Updating and upgrading machine, please wait...", "white"))
-        process = subprocess.run(["yes | sudo apt update && yes | sudo apt upgrade"], shell=True, capture_output=True, text=True)
+        process = subprocess.run(["sudo apt update -y && sudo apt upgrade -y"], shell=True, capture_output=True, text=True)
         print(colored((process.stderr), "red"))
     else:
         print(colored("***Skipping APT Update/Upgrade***", "green"))
