e656519f4db6c88ff9044ad07a09c86aec9591ed
safemoad <safemoad+github@proton.me>
2025-06-14 17:05:22 -0700

    Update prep_host.py

--- automation/prep_host.py
+++ automation/prep_host.py
@@ -1,24 +1,32 @@
 # Import required modules
 import subprocess
+from termcolor import colored
 
-# Update OS and Packages
-read -n 1 -p 'Step 1 of 3: Run APT Update/Upgrade on this machine? [Y/n] ' response
-  if [ "$response" == "Y" ] 
-    then
-      echo ''
-      echo 'Step 1 of 3: Updating and upgrading machine, please wait... ' && yes | sudo apt update > /dev/null 2>&1 && yes | sudo apt upgrade > /dev/null 2>&1
-    else
-      echo ''
-      echo '***Skipping APT Update/Upgrade*** '
-    fi
+#Update OS and Packages
+def update_apt():
+    response = input("Step 1 of 3: Run APT Update/Upgrade on this machine? [Y/n]")
+    if response == "Y":
+        print(colored("Step 1 of 3: Updating and upgrading machine, please wait...", "white"))
+        process = subprocess.run(["yes | sudo apt update && yes | sudo apt upgrade"], shell=True, capture_output=True, text=True)
+        print(colored((process.stderr), "red"))
+    else:
+        print(colored("***Skipping APT Update/Upgrade***", "green"))
 
-# Update Searchsploit Database
-echo 'Step 2 of 3: Updating Searchsploit database, please wait... '
-searchsploit -u > /dev/null 2>&1
+#Update Searchsploit Database
+def update_searchsploit():
+    print(colored("Step 2 of 3: Updating Searchsploit database, please wait...", "white"))
+    process = subprocess.run(["searchsploit -u"], shell=True, capture_output=True, text=True)
+    print(colored((process.stderr), "red"))
 
 # Update Locate Database
-echo 'Step 3 of 3: Updating Locate database, please wait... '
-sudo updatedb > /dev/null 2>&1
+def update_locate():
+    print(colored("Step 3 of 3: Updating Locate database, please wait...", "white"))
+    process = subprocess.run(["sudo updatedb"], shell=True, capture_output=True, text=True)
+    print(colored((process.stderr), "red"))
 
-# Finished
-echo '***Host prep complete!*** '
+# Main
+if __name__ == "__main__":
+    update_apt()
+    update_searchsploit()
+    update_locate()
+    print(colored("***Host prep complete!***", "green"))
