ccc98bcf9636c93a4eb31d2ceed628ed26fd3a7c
safemoad <safemoad+github@proton.me>
2025-06-14 17:03:10 -0700

    Update enum_target.py

--- automation/enum_target.py
+++ automation/enum_target.py
@@ -8,14 +8,14 @@ RHOST = ""
 def get_RHOST():
   global RHOST
   while True:
-        target_response = input("Enter the target IP address (e.g., 192.168.1.1 or example.com):")
+        response = input("Enter the target IP address (e.g., 192.168.1.1 or example.com):")
 
         # Basic validation for IP address or hostname format
         ip_pattern = re.compile(r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
         hostname_pattern = re.compile(r"^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
 
-        if ip_pattern.match(target_response) or hostname_pattern.match(target_response):
-            RHOST = target_response
+        if ip_pattern.match(response) or hostname_pattern.match(response):
+            RHOST = response
             print(f"Target '{RHOST}' acquired! ")
             break
         else:
